727ab416de253774dda0d37f0434298defe38da7
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2023-01-25 17:43:59 -0500

    code-a-long complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,8 +7,18 @@ class MoviesController < ApplicationController
   end
 
   def show
-    movie = Movie.find(params[:id])
+      movie = Movie.find(params[:id])
     render json: movie
+  end
+
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
   end
 
   private
